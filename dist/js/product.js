d3c7420ecd5e23102277f02600e052ac91db3946
Abdalla Taha <98702803+Abdullah-Taha07@users.noreply.github.com>
2025-10-28 19:41:56 +0300

    Updates

--- dist/js/product.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(self["webpackChunkcoffee_company"] = self["webpackChunkcoffee_company"] || []).push([[964],{},
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ __webpack_require__.O(0, [599,891], () => (__webpack_exec__(863)));
-/******/ var __webpack_exports__ = __webpack_require__.O();
-/******/ }
-]);
